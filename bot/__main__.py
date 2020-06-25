7fad852382d54981947f91ca08489d0747acf280
samhallam03 <92414@farnborough.ac.uk>
2020-06-25 16:24:30 +0100

    added game status

--- bot/__main__.py
+++ bot/__main__.py
@@ -15,6 +15,10 @@ bot = commands.Bot(
     case_insensitive=True
 )
 
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name=f'in {len(bot.guilds)} servers.'))
+
 bot.load_extension('bot.cogs.player')
 bot.load_extension('bot.cogs.server')
 
